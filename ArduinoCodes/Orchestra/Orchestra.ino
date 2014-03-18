e774b7c3b5aa37e1f39bb6f01330e84665253769
Chi Liu <ichase.liu@gmail.com>
2014-03-17 21:42:38 -0400

    Update Orchestra.ino

--- ArduinoCodes/Orchestra/Orchestra.ino
+++ ArduinoCodes/Orchestra/Orchestra.ino
@@ -1,17 +1,17 @@
 #define n_input 2
 
 int pingPins[n_input];
-int sonarPins[n_input];
+int potentialPins[n_input];
 int sensorData[n_input*2];
 
 void setup() {
-  int i = 0;
-  for (i = 0; i < n_input; i++){
-    pingPings[i] = i;
-    sonarPins[i] = 7 + i;
-  }
+    int i = 0;
+    for (i = 0; i < n_input; i++) {
+        pingPins[i] = i;
+        sonarPins[i] = 7 + i;
+    }
 }
 
 void loop() {
-  
+    
 }
