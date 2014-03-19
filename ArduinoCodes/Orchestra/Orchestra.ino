0640b8e76b62ef4082bc05b89e769554296912e0
Pranay-Agrawal <pranayprank@gmail.com>
2014-03-18 22:41:52 -0400

    Update Orchestra.ino

--- ArduinoCodes/Orchestra/Orchestra.ino
+++ ArduinoCodes/Orchestra/Orchestra.ino
@@ -1,117 +1,256 @@
-#define N_INPUT 2
-#define APIN_START 0
-#define DPIN_START 7
+/*
+ 2-12-2011
+ Spark Fun Electronics 2011
+ Nathan Seidle
+ Updated to Arduino 1.01 by Marc "Trench" Tschudin
+ 
+ This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
+ 
+ This code works with the VS1053 Breakout Board and controls the VS1053 in what is called Real Time MIDI mode. 
+ To get the VS1053 into RT MIDI mode, power up the VS1053 breakout board with GPIO0 tied low, GPIO1 tied high.
+ 
+ I use the NewSoftSerial library to send out the MIDI serial at 31250bps. This allows me to print regular messages
+ for debugging to the terminal window. This helped me out a ton.
+ 
+ 5V : VS1053 VCC
+ GND : VS1053 GND
+ D3 (SoftSerial TX) : VS1053 RX
+ D4 : VS1053 RESET
+ 
+ Attach a headphone breakout board to the VS1053:
+ VS1053 LEFT : TSH
+ VS1053 RIGHT : RSHconst
+ VS1053 GBUF : GND
+ 
+ When in the drum bank (0x78), there are not different instruments, only different notes.
+ To play the different sounds, select an instrument # like 5, then play notes 27 to 87.
+ 
+ To play "Sticks" (31):
+ talkMIDI(0xB0, 0, 0x78); //Bank select: drums
+ talkMIDI(0xC0, 5, 0); //Set instrument number
+ //Play note on channel 1 (0x90), some note value (note), middle velocity (60):
+ noteOn(0, 31, 60);
+ 
+ */
 
 #include <SoftwareSerial.h>
 
-int potential_pins[N_INPUT];
-int ping_pins[N_INPUT];
-int potential_read[N_INPUT];
-int ping_read[N_INPUT];
-int volumns[N_INPUT];
-int notes[N_INPUT];
+const int pingPin1 = 7;
+const int pingPin2 = 8;
 
-int i = 0;
+//const int volume1 = 0;
+//const int volume1 = 1;
 
-// ping sensor variables
-int duration, cm;
-
-// Music shield variables
 SoftwareSerial mySerial(2, 3); // RX, TX
+
 byte note = 0; //The MIDI note value to be played
 byte resetMIDI = 4; //Tied to VS1053 Reset line
 byte ledPin = 13; //MIDI traffic inidicator
 int  instrument = 0;
 
+
 void setup() {
-    for (i = 0; i < N_INPUT; i++) {
-        potential_pins[i] = APIN_START + i;
-        ping_pins[i] = DPIN_START + i;
-    }
+  Serial.begin(57600);
 
-    //Setup soft serial for MIDI control
-    mySerial.begin(31250);
-
-    //Reset the VS1053
-    pinMode(resetMIDI, OUTPUT);
-    digitalWrite(resetMIDI, LOW);
-    delay(100);
-    digitalWrite(resetMIDI, HIGH);
-    delay(100);
-    talkMIDI(0xB0, 0x07, 120); //0xB0 is channel message, set channel volume to near max (127)
+  //Setup soft serial for MIDI control
+  mySerial.begin(31250);
+  
+  //Reset the VS1053
+  pinMode(resetMIDI, OUTPUT);
+  digitalWrite(resetMIDI, LOW);
+  delay(100);
+  digitalWrite(resetMIDI, HIGH);
+  delay(100);
+  talkMIDI(0xB0, 0x07, 120); //0xB0 is channel message, set channel volume to near max (127)
 }
 
 void loop() {
-    getVolumns();
-    getNotes();
-    playMusic();
-}
 
-void getVolumns() {
-    for (i = 0; i < N_INPUT; i++) {
-        potential_read[i] = analogRead(potential_pins[i]);
-        volumns = map(potential_read[i], 0, 1024, 0, 128);
+  long duration1, inches1, cm1,duration2, inches2, cm2, vol1, vol2;
+
+/*
+  //Demo Basic MIDI instruments, GM1
+  //=================================================================
+  Serial.println("Basic Instruments");
+  talkMIDI(0xB0, 0, 0x00); //Default bank GM1
+
+  //Change to different instrument
+  for(instrument = 0 ; instrument < 127 ; instrument++) {
+
+    Serial.print(" Instrument: ");
+    Serial.println(instrument, DEC);
+
+    talkMIDI(0xC0, instrument, 0); //Set instrument number. 0xC0 is a 1 data byte command
+
+    //Play notes from F#-0 (30) to F#-5 (90):
+    for (note = 30 ; note < 40 ; note++) {
+      Serial.print("N:");
+      Serial.println(note, DEC);
+      
+      //Note on channel 1 (0x90), some note value (note), middle velocity (0x45):
+      noteOn(0, note, 60);
+      delay(50);
+
+      //Turn off the note with a given off/release velocity
+      noteOff(0, note, 60);
+      delay(50);
     }
-}
 
-void getNotes() {
-    for (i = 0; i < N_INPUT; i++) {
-        ping_read[i] = getRange(ping_pins[i]);
-        notes[i] = map(ping_read[i], 10, 60, 60, 90);
+    delay(100); //Delay between instruments
+  }
+  //=================================================================
+
+*/
+
+/*
+
+  //Demo GM2 / Fancy sounds
+  //=================================================================
+  Serial.println("Demo Fancy Sounds");
+  talkMIDI(0xB0, 0, 0x78); //Bank select drums
+
+  //For this bank 0x78, the instrument does not matter, only the note
+  for(instrument = 30 ; instrument < 31 ; instrument++) {
+
+    Serial.print(" Instrument: ");
+    Serial.println(instrument, DEC);
+
+    talkMIDI(0xC0, instrument, 0); //Set instrument number. 0xC0 is a 1 data byte command
+
+    //Play fancy sounds from 'High Q' to 'Open Surdo [EXC 6]'
+    for (note = 27 ; note < 87 ; note++) {
+      Serial.print("N:");
+      Serial.println(note, DEC);
+      
+      //Note on channel 1 (0x90), some note value (note), middle velocity (0x45):
+      noteOn(0, note, 60);
+      delay(100);
+
+      //Turn off the note with a given off/release velocity
+      noteOff(0, note, 60);
+      delay(100);
     }
-}
 
-int getRange(int pingPin) {
-    pinMode(pingPin, OUTPUT);
-    digitalWrite(pingPin, LOW);
+    delay(100); //Delay between instruments
+  }
+
+*/
+
+
+  //Demo Melodic
+  //=================================================================
+  //Serial.println("Demo Melodic? Sounds");
+  talkMIDI(0xB0, 0, 0x79); //Bank select Melodic
+  //These don't sound different from the main bank to me
+
+  //Change to different instrument
+  //for(instrument = 27 ; instrument < 87 ; instrument++) {
+
+    //Serial.print(" Instrument: ");
+    //Serial.println(instrument, DEC);
+
+
+    //Play notes from F#-0 (30) to F#-5 (90):
+    //for (note = 30 ; note < 90 ; note++) {
+    
+    vol1 = analogRead(0);
+    vol2 = analogRead(1);  
+    
+//    Serial.println(vol1);
+//  Serial.println(vol2);  
+      
+    pinMode(pingPin1, OUTPUT);
+    digitalWrite(pingPin1, LOW);
     delayMicroseconds(2);
-    digitalWrite(pingPin, HIGH);
+    digitalWrite(pingPin1, HIGH);
     delayMicroseconds(5);
-    digitalWrite(pingPin, LOW);
-    pinMode(pingPin, INPUT);
-    duration = pulseIn(pingPin, HIGH);
-    return duration / 29 / 2;
+    digitalWrite(pingPin1, LOW);
+    
+    pinMode(pingPin1, INPUT);
+    duration1 = pulseIn(pingPin1, HIGH);
+  
+    cm1 = microsecondsToCentimeters(duration1);
+    Serial.print(cm1);
+    Serial.println("cm1");
+    
+    pinMode(pingPin2, OUTPUT);
+    digitalWrite(pingPin2, LOW);
+    delayMicroseconds(2);
+    digitalWrite(pingPin2, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(pingPin2, LOW);
+  
+    pinMode(pingPin2, INPUT);
+    duration2 = pulseIn(pingPin2, HIGH);
+  
+    cm2 = microsecondsToCentimeters(duration2);
+    Serial.print(cm2);
+    Serial.println("cm2");
+      
+      //Serial.print("N:");
+      //Serial.println(note, DEC);
+      
+      //Note on channel 1 (0x90), some note value (note), middle velocity (0x45):
+      talkMIDI(0xC0, 27, 0); //Set instrument number. 0xC0 is a 1 data byte command
+      noteOn(0, cm1+30, vol1*90/1023);
+      
+      talkMIDI(0xC0, 18, 0); //Set instrument number. 0xC0 is a 1 data byte command
+      noteOn(0, cm2+30, vol2*90/1023);
+      
+      delay(1000);
+
+      //Turn off the note with a given off/release velocity
+      noteOff(0, cm1+30 ,  vol1*90/1023);
+      noteOff(0, cm2+30 , vol2*90/1023);
+      //delay(200);
+    //}
+
+    //delay(100); //Delay between instruments
+  //}
 
 
-void playMusic() {
-    talkMIDI(0xB0, 0, 0x79); //Bank select melodies
 
-    //For this bank 0x78, the instrument does not matter, only the note
-    for(i = 0 ; i < N_INPUT ; i++) {
-        talkMIDI(0xB0, 0x07, volumns[i]);  // Set channel volumn
-
-        talkMIDI(0xC0, 27 + 5*i, 0); //Set instrument number. 0xC0 is a 1 data byte command
-        //Note on channel 1 (0x90), some note value (note), middle velocity (0x45):
-        noteOn(0, notes[i], 60);
-        delay(50);
-
-        //Turn off the note with a given off/release velocity
-        noteOff(0, notes[i], 60);
-        delay(50);
-    }
 }
 
 //Send a MIDI note-on message.  Like pressing a piano key
 //channel ranges from 0-15
 void noteOn(byte channel, byte note, byte attack_velocity) {
-    talkMIDI( (0x90 | channel), note, attack_velocity);
+  talkMIDI( (0x90 | channel), note, attack_velocity);
 }
 
 //Send a MIDI note-off message.  Like releasing a piano key
 void noteOff(byte channel, byte note, byte release_velocity) {
-    talkMIDI( (0x80 | channel), note, release_velocity);
+  talkMIDI( (0x80 | channel), note, release_velocity);
 }
 
 //Plays a MIDI note. Doesn't check to see that cmd is greater than 127, or that data values are less than 127
 void talkMIDI(byte cmd, byte data1, byte data2) {
-    digitalWrite(ledPin, HIGH);
-    mySerial.write(cmd);
-    mySerial.write(data1);
+  digitalWrite(ledPin, HIGH);
+  mySerial.write(cmd);
+  mySerial.write(data1);
 
-    //Some commands only have one data byte. All cmds less than 0xBn have 2 data bytes 
-    //(sort of: http://253.ccarh.org/handout/midiprotocol/)
-    if( (cmd & 0xF0) <= 0xB0)
-        mySerial.write(data2);
+  //Some commands only have one data byte. All cmds less than 0xBn have 2 data bytes 
+  //(sort of: http://253.ccarh.org/handout/midiprotocol/)
+  if( (cmd & 0xF0) <= 0xB0)
+    mySerial.write(data2);
 
-    digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin, LOW);
+}
+
+long microsecondsToInches(long microseconds)
+{
+  // According to Parallax's datasheet for the PING))), there are
+  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
+  // second).  This gives the distance travelled by the ping, outbound
+  // and return, so we divide by 2 to get the distance of the obstacle.
+  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
+  return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
 }
